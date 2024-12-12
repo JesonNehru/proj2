9a87ce66f3193ee4d311d8a588dab4ff26e2c44d
JesonNehru <jesonsaral56@gmail.com>
2024-12-12 00:58:54 -0500

    Update cognito-auth.js

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -74,6 +74,7 @@ var WildRydes = window.WildRydes || {};
         var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
             Username: toUsername(email),
             Password: password
+            SecretHash: secretHash
         });
 
         var cognitoUser = createCognitoUser(email);
