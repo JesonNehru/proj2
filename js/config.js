64506e0ef78215be7ffe37512bf97056e8ddcc46
JesonNehru <jesonsaral56@gmail.com>
2024-12-12 01:15:08 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,8 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'ca-central-1_8s2qrsIUr', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3d8sounpq1sli2qkofh75pijkq', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ca-central-1' // e.g. us-east-2
-    },
+        region: 'ca-central-1', // e.g. us-east-2
+        SecretHash: '1r1ujmk97e8m5106idacum2m9otmi8utik0l07vhn1nnqde4bn5f'
+},
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
